68ad5650b5a1c8eba94aaab4663c52e12ff17439
itdaos <jjok730@gmail.com>
2020-07-07 19:42:32 +0300

    Mobile fixes 1

--- tabs.js
+++ tabs.js
@@ -18,27 +18,27 @@ $(document).ready(function() {
   });
 
   $("#cab-trigger-1").click(function () {
-    $("#cab-tabs-1").removeClass("d-md-none d-lg-none d-xl-none");
-    $("#cab-tabs-2").addClass("d-md-none d-lg-none d-xl-none");
-    $("#cab-tabs-3").addClass("d-md-none d-lg-none d-xl-none");
+    $("#cab-tabs-1").removeClass("d-none");
+    $("#cab-tabs-2").addClass("d-none");
+    $("#cab-tabs-3").addClass("d-none");
     $("#cab-trigger-1").addClass("t-active");
     $("#cab-trigger-2").removeClass("t-active");
     $("#cab-trigger-3").removeClass("t-active");
   });
 
   $("#cab-trigger-2").click(function () {
-    $("#cab-tabs-2").removeClass("d-md-none d-lg-none d-xl-none");
-    $("#cab-tabs-1").addClass("d-md-none d-lg-none d-xl-none");
-    $("#cab-tabs-3").addClass("d-md-none d-lg-none d-xl-none");
+    $("#cab-tabs-2").removeClass("d-none");
+    $("#cab-tabs-1").addClass("d-none");
+    $("#cab-tabs-3").addClass("d-none");
     $("#cab-trigger-2").addClass("t-active");
     $("#cab-trigger-1").removeClass("t-active");
     $("#cab-trigger-3").removeClass("t-active");
   });
 
   $("#cab-trigger-3").click(function () {
-    $("#cab-tabs-3").removeClass("d-md-none d-lg-none d-xl-none");
-    $("#cab-tabs-2").addClass("d-md-none d-lg-none d-xl-none");
-    $("#cab-tabs-1").addClass("d-md-none d-lg-none d-xl-none");
+    $("#cab-tabs-3").removeClass("d-none");
+    $("#cab-tabs-2").addClass("d-none");
+    $("#cab-tabs-1").addClass("d-none");
     $("#cab-trigger-3").addClass("t-active");
     $("#cab-trigger-2").removeClass("t-active");
     $("#cab-trigger-1").removeClass("t-active");
