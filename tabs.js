8b9a3578f9d79429db7302b5fea61d71b2b69d3e
itdaos <jjok730@gmail.com>
2020-07-10 22:31:32 +0300

    Fixes 4

--- tabs.js
+++ tabs.js
@@ -70,43 +70,6 @@ $(document).ready(function() {
     $("#m-cab-trigger-2").removeClass("t-active");
     $("#m-cab-trigger-1").removeClass("t-active");
   });
+
+  
 });
-
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
-
-$('#closed').click(false)
